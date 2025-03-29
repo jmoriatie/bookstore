d7c405b2d4677f73db7e3e1cafffc02dc3758f2c
jeongmin.kim <jeongmin.kim@yanolja.com>
2025-03-30 01:47:59 +0900

    [테스트 환경 설정] - 테스트 yml 설정 - junit에 Slf4J 활용 가능하도록 gradle 설정 추가

---
+++ src/test/java/com/solve/bookstore/application/BookServiceTest.java
@@ -0,0 +1,12 @@
+package com.solve.bookstore.application;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookServiceTest {
+
+    @Test
+    void changeCategory() {
+    }
+}
